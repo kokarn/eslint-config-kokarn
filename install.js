d39816ca859602f980913975f9e307169244643c
Oskar Risberg <kokarn@gmail.com>
2017-01-09 20:59:48 +0100

    Add basic install script

---
+++ install.js
@@ -0,0 +1,29 @@
+const fs = require( 'fs' );
+const path = require( 'path' );
+
+const DEFAULT_CONFIG = {
+    extends: 'kokarn'
+};
+const DEFAULT_TEST_COMMAND = 'eslint *.js';
+const DEFAULT_ENGINE = '^6.9.4';
+const PACKAGE_PATH = path.join( __dirname, '../../', 'package.json' );
+
+const packageData = fs.readFileSync( PACKAGE_PATH, 'utf8' );
+
+if( !packageData.scripts ){
+    packageData.scripts = {
+        pretest: DEFAULT_TEST_COMMAND
+    };
+} else if ( packageData.scripts && !packageData.scripts.pretest ){
+    packageData.scripts.pretest = DEFAULT_TEST_COMMAND;
+}
+
+if ( !packageData.engines ){
+    packageData.engines = {
+        node: DEFAULT_ENGINE
+    };
+}
+
+packageData.eslintConfig = DEFAULT_CONFIG;
+
+fs.writeFileSync( PACKAGE_PATH, JSON.stringify( packageData, null, 2 ) );
