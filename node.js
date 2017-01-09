80623328bfa5f21032a7d1acc274c734fa611e43
Oskar Risberg <kokarn@gmail.com>
2017-01-09 21:56:34 +0100

    Bump eslint-plugin-node to 3.*

--- node.js
+++ node.js
@@ -13,13 +13,35 @@ module.exports = {
         'no-console': [
             'off'
         ],
+        'node/exports-style': [
+            'error',
+            'module.exports'
+        ],
+        'node/no-deprecated-api': [
+            'error'
+        ],
         'node/no-missing-import': [
+            'error'
+        ],
+        'node/no-missing-require': [
+            'error'
+        ],
+        'node/no-unpublished-bin': [
             'error'
         ],
         'node/no-unpublished-import': [
             'error'
         ],
+        'node/no-unpublished-require': [
+            'error'
+        ],
+        'node/no-unsupported-features': [
+            'error'
+        ],
         'node/process-exit-as-throw': [
+            'error'
+        ],
+        'node/shebang': [
             'error'
         ]
     }
